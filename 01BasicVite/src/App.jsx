6cf9928316f558f7692ff39db545049bc60c6972
Soham Ghag <sohamg640@gmail.com>
2025-02-12 20:53:37 +0530

    Soham Doc

---
+++ 01BasicVite/src/App.jsx
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube.jsx";
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Youtube />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01BasicVite/src/Youtube.jsx
@@ -0,0 +1,10 @@
+function Youtube() {
+  return (
+    <>
+      <h1>Youtube</h1>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default Youtube;
